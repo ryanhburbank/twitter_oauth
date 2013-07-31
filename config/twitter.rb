b7a82109c8913edba1d3edaa9ee1345983215d24
A Tall American <ryanhburbank@gmail.com>
2013-07-31 10:17:15 -0500

    Delete twitter.rb

--- config/twitter.rb
+++
@@ -1,7 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = "OKlL5RRYIkYGyHS1rrd1g"
-  config.consumer_secret = "kkXLUNI3ZkDS95OFNJ3y000ALVyRxOQ6aygMc6Io9DM"
-end
-
-ENV['TWITTER_KEY'] = "OKlL5RRYIkYGyHS1rrd1g"
-ENV['TWITTER_SECRET'] ="kkXLUNI3ZkDS95OFNJ3y000ALVyRxOQ6aygMc6Io9DM"
